8675c78fb96f68f364d2006debf927c7a3b41c54
UnkindlyBiased <inoutoftheverge@gmail.com>
2024-01-13 15:05:16 +0200

    Map is working

---
+++ app/src/main/java/com/litekreu/utaxi/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.litekreu.utaxi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.litekreu.utaxi.presentation.MainScreen
+import com.litekreu.utaxi.ui.theme.UTaxiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            UTaxiTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/litekreu/utaxi/presentation/MainScreen.kt
@@ -0,0 +1,13 @@
+package com.litekreu.utaxi.presentation
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.google.maps.android.compose.GoogleMap
+
+@Composable
+fun MainScreen() {
+    GoogleMap(modifier = Modifier.fillMaxSize()) {
+
+    }
+}
